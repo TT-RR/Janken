f701b6cb1c7ff2ad2c77f38c644ca2a42cebe772
TT-RR <118715539+TT-RR@users.noreply.github.com>
2022-11-22 10:09:54 +0900

    Add files via upload

---
+++ Janken/JankenHand.cs
@@ -0,0 +1,9 @@
+﻿namespace Janken  //Jankenという新しい型の作成
+{
+    enum JankenHand //列挙型の定義
+    {
+        Rock,
+        Scissors,
+        Paper
+    }
+}
---
+++ Janken/main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Janken
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void rookHand_Click(object sender, EventArgs e)
+        //グーをクリックすると、
+        {
+            var hand = JankenHand.Rock;
+            //プレイヤーの手をグーにする
+
+            Image handImage;
+            if(hand==JankenHand.Rock)
+            {
+                handImage = Properties.Resources.rock;
+            }
+            else if (hand == JankenHand.Scissors)
+            {
+                handImage = Properties.Resources.scissors;
+            }
+            else
+            {
+                handImage = Properties.Resources.paper;
+            }
+            //押された手に応じた画像をプレイヤーの手にする
+            prayerHand.Image = handImage;
+        }
+
+    }
+}
