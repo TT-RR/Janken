94d873e6cdfd6ff24d5711f0e6b2606e25f01b96
TT-RR <118715539+TT-RR@users.noreply.github.com>
2022-11-30 10:59:25 +0900

    7章完成
    プログレスバーの名前を教科書と変更
    Life.csの変数名ほとんど変更

---
+++ Janken/Life.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Janken
+{
+    class Life
+    {
+        private ProgressBar progressBar;
+
+        public Life(ProgressBar progressBar,int maxLife)
+        {
+            //初期化
+            this.progressBar = progressBar;
+            this.progressBar.Maximum = maxLife;
+            //HPを減らす機能
+            this.progressBar.Step = -1;
+        }
+
+        public void Attack()
+        //Attackが呼び出されたら、PerformStep実行
+        {
+            this.progressBar.PerformStep();
+        }
+
+        public bool Alive => this.progressBar.Value > 0;
+        //AliveプロパティでHPバーが0より大きいか判定する
+        //=>とは構造体のポインタと同じ使い方
+
+
+        public void Reset()
+        {
+            //HPバーのゲージリセット
+            this.progressBar.Value = this.progressBar.Maximum;
+        }
+    }
+}
